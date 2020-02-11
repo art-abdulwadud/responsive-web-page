af319e7623b03cf36463fcf00f66ab82991432d8
maserati12 <triggamaserati@gmail.com>
2020-02-11 10:24:28 +0300

    menu navbar

--- responsive.js
+++ responsive.js
@@ -21,6 +21,15 @@ openMenu.addEventListener('click', () => {
 	sidebarActive = true;
 	for(let i = 0; i < links.length; i++){
 		links[i].style.display = 'inline-block';
+		links[i].addEventListener('click', () => {
+			for(let j = 0; j < links.length; j++){
+				links[j].style.display = 'none';
+			}
+			mainGrid.classList.remove('main-grid-active')
+			mainGrid.classList.add('main-grid')
+			closeMenu.style.display = 'none';
+			openMenu.style.display = 'inline-table';
+		})
 	}
 	mainGrid.classList.remove('main-grid')
 	mainGrid.classList.add('main-grid-active')
